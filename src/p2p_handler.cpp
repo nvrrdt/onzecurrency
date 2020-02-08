929f87f4a87469df43285ad720f515eaead51fe3
nvrrdt <nvrrdt@gmail.com>
2020-02-08 11:40:20 +0100

    Put p2p_switch in a thread

--- src/p2p_handler.cpp
+++ src/p2p_handler.cpp
@@ -8,22 +8,34 @@
 #include "json.hpp"
 
 #include "p2p_handler.hpp"
-  
+
+#include <future>
+#include <thread>
+
 using namespace crowd;
 
 void p2p_handler::p2p_switch()
 {
-    p2p_handler peers;
-    vector<string> ip_list = peers.parse_ip_peers_json();
+    // Create a packaged_task using some task and get its future.
+    std::packaged_task<void()> task([] {
+        p2p_handler peers;
+        vector<string> ip_list = peers.parse_ip_peers_json();
 
-    for (string& ip_adress : ip_list)
-    {
-        p2p_handler cl;
-        cl.client(ip_adress);
-    }
+        for (string& ip_adress : ip_list)
+        {
+            p2p_handler cl;
+            cl.client(ip_adress);
+        }
 
-    p2p_handler se;
-    se.server_main();
+        p2p_handler se;
+        se.server_main();
+    });
+    auto future = task.get_future();
+
+    // Run task on new thread.
+    std::thread t(std::move(task));
+
+    t.join();
 }
 
 vector<string> p2p_handler::parse_ip_peers_json() // https://github.com/nlohmann/json
