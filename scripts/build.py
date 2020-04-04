3b4184b15be31e97ea2a4b9bee50f07f1ad94599
nvrrdt <nvrrdt@gmail.com>
2020-04-04 04:50:45 +0200

    Adding keyboardinterrupt

--- scripts/build.py
+++ scripts/build.py
@@ -40,4 +40,12 @@ def main():
     else:
         print("There's something wrong!")
 
-main()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
