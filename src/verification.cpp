8d7bcf5179575e3e6cd36d070da2321280823251
nvrrdt <nvrrdt@gmail.com>
2020-04-21 19:27:07 +0200

    Preparation for further finalising Verification class

---
+++ src/verification.cpp
@@ -0,0 +1,37 @@
+#include "verification.hpp"
+
+Verification::Verification()
+{
+    // blockchain folder is_empty; download
+    // blockchain folder not_empty; what is latest block
+    // latest block not_latest; update
+    
+    //std::cout << "blockchain_folder_path: " << blockchain_folder_path << std::endl;
+    boost::system::error_code c;
+    boost::filesystem::path path(blockchain_folder_path);
+
+    if (!boost::filesystem::exists(path))
+    {
+        boost::filesystem::create_directory(path);
+        Verification::Download();
+    }
+    else
+    {
+        bool isEmpty = boost::filesystem::is_empty(path);
+        if (isEmpty)
+        {
+            Verification::Download();
+        }
+    }
+
+    Verification::Update();
+}
+bool Verification::Download()
+{
+    // Add 1 to own user_id and search for next chosen_one in leveldb that is online
+}
+bool Verification::Update()
+{
+    // check for the latest block in blockchain folder and verify if that's the latest in the chain
+    // if not the latest: update
+}
