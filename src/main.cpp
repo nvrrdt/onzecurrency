cabf0b8ce9043b9417375be3e724401e46d75905
nvrrdt <nvrrdt@gmail.com>
2020-08-23 17:02:54 +0200

    Add p2p_handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <onzehub/auth.hpp>
+#include <onzehub/p2p.hpp>
 
 #include "main.hpp"
 
@@ -8,6 +9,9 @@ int main(int argc, char *argv[])
 {
     auth a;
     a.authentication();
+
+    p2p_handler ph;
+    ph.p2p_switch("download", "172.31.24.198");
 
     return 0;
 }
