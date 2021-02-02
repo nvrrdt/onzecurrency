56f27dac2236640236a84ff236d3b328d5a75d5e
nvrrdt <nicoverrijdt@gmail.com>
2021-02-02 07:33:05 +0100

    Example code for the rsa encryption, not complete, the localkeypair is for instance not read from a file

--- include/libcommon/crypto_rsa.hpp
+++ include/libcommon/crypto_rsa.hpp
@@ -5,6 +5,7 @@
 namespace Crowd
 {
     // example: https://shanetully.com/2012/06/openssl-rsa-aes-and-c/
+    // or/and https://github.com/shanet/Crypto-Example
     class Rsa
     {
     public:
--- src/libcommon/crypto_rsa.cpp
+++ src/libcommon/crypto_rsa.cpp
@@ -1,1 +1,2 @@
 // example: https://shanetully.com/2012/06/openssl-rsa-aes-and-c/
+// or/and https://github.com/shanet/Crypto-Example
