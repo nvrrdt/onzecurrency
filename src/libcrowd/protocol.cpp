337b5e997dfabc90f85de4a5e647233c25a26991
nvrrdt <nicoverrijdt@gmail.com>
2021-02-08 08:53:40 +0100

    Small cleanup to present it better

--- src/libcrowd/protocol.cpp
+++ src/libcrowd/protocol.cpp
@@ -44,16 +44,16 @@ std::string Protocol::latest_block()
 std::map<uint32_t, uint256_t> Protocol::layers_management(uint256_t &amount_of_peers)
 {
     const int nmax = 100;   // max number of peers per section
-    uint32_t i;             // number of layers
+    uint32_t maxreallayers;             // number of layers
     uint256_t tmaxmax = 0, tmaxmin = 0, overshoot = 0, tlayer = 0;
 
     std::vector<uint256_t> layercontents;
 
     int imax = 64;
-    for (i = 1; i <= imax; i++)
+    for (maxreallayers = 1; maxreallayers <= imax; maxreallayers++)
     {
         
-        tlayer = uint256_t(pow(nmax, i));   // max number of peers in a layer
+        tlayer = uint256_t(pow(nmax, maxreallayers));   // max number of peers in a layer
         tmaxmax += tlayer;                  // max number of peers in all layers
 
         layercontents.push_back(tlayer);
@@ -62,25 +62,25 @@ std::map<uint32_t, uint256_t> Protocol::layers_management(uint256_t &amount_of_p
         {
             // std::cout << std::endl << "max peers last layer: " << tlayer << std::endl;
             // std::cout << "max peers all layers: " << tmaxmax << std::endl;
-            std::cout << std::endl << "amount of layers: " << i << std::endl;
+            std::cout << std::endl << "amount of layers: " << maxreallayers << std::endl;
             std::cout << "amount_of_peers: " << amount_of_peers << std::endl;
             break;
         }
-        else if (i == imax && amount_of_peers > tmaxmax)
+        else if (maxreallayers == imax && amount_of_peers > tmaxmax)
         {
             std::cerr << "ERROR: change imax as there are more peers than imax can handle" << std::endl;
         }
     }
 
-    tmaxmin = tmaxmax - layercontents.at(i-1);      // max number of peers in all layers minus last layer
-    overshoot = amount_of_peers - tmaxmin;    // number of peers in last layer
+    tmaxmin = tmaxmax - layercontents.at(maxreallayers-1);  // max number of peers in all layers minus last layer
+    overshoot = amount_of_peers - tmaxmin;                  // number of peers in last layer
     
     // partitioning of last layer:
     // explanation: (remainder) contains (quotient + 1) peers in 1 bucket of last layer
     //              (layercontents(last_layer) - remainder) contains (quotient) peers in 1 bucket of last layer
     //              (layercontents(last_layer)) is maximum number of peers in that layer
     uint256_t quotient, remainder;
-    if (i == 1) // only 1 layer == layer 0
+    if (maxreallayers == 1) // only 1 layer == layer 0
     {
         quotient = 0;
         remainder = overshoot;
@@ -89,71 +89,72 @@ std::map<uint32_t, uint256_t> Protocol::layers_management(uint256_t &amount_of_p
     }
     else
     {
-        quotient = overshoot / layercontents.at(i-1);
-        remainder = overshoot % layercontents.at(i-1);
-        // std::cout << remainder << " buckets in last layer with " << quotient+1 << " peers and " << (layercontents.at(i-2))-remainder << " buckets with " << quotient << " peers." << std::endl;
-        // std::cout << overshoot << " (overshoot) must be equal to " << (remainder*(quotient+1))+((layercontents.at(i-2)-remainder)*quotient) << " (calculation of here before mentioned numbers)" << std::endl;
+        quotient = overshoot / layercontents.at(maxreallayers-1);
+        remainder = overshoot % layercontents.at(maxreallayers-1);
+        // std::cout << remainder << " buckets in last layer with " << quotient+1 << " peers and " << (layercontents.at(maxreallayers-2))-remainder << " buckets with " << quotient << " peers." << std::endl;
+        // std::cout << overshoot << " (overshoot) must be equal to " << (remainder*(quotient+1))+((layercontents.at(maxreallayers-2)-remainder)*quotient) << " (calculation of here before mentioned numbers)" << std::endl;
     }
 
-    // std::cout << "numberlayers: " << i << std::endl;
+    // std::cout << "numberlayers: " << maxreallayers << std::endl;
     // std::cout << "numbermaxbucketslastlayer: " << remainder << std::endl;
     // std::cout << "contentsmaxbucketslastlayer: " << quotient+1 << std::endl;
-    // std::cout << "numberminbucketslastlayer: " << ((i == 1) ? 100-remainder : (layercontents.at(i-1)-remainder)) << std::endl;
+    // std::cout << "numberminbucketslastlayer: " << ((maxreallayers == 1) ? 100-remainder : (layercontents.at(maxreallayers-1)-remainder)) << std::endl;
     // std::cout << "contentsminbucketslastlayer: " << quotient << std::endl;
     // std::cout << "overshootlastlayer: " << overshoot << std::endl;
 
-    // the peers between every top layer peer is counted and put in a vector
+    // the peers between every top layer peer are counted and put in a map
     uint256_t counter, total_counter;
     std::map<uint32_t, uint256_t> chosen_ones_counter;
-    for (int k = 1; k <= nmax; k++)
+
+    for (int i = 1; i <= nmax; i++) // nmax = amoutn of chosen ones
     {
         chosen_ones_counter[i] = 0;
     }
-    for (int k = 1; k <= nmax; k++)
+    for (int i = 1; i <= nmax; i++)
     {
-        for (int layer = 1; layer <= imax; layer++)
+        for (int layer = 1; layer <= maxreallayers; layer++)
         {
-            if (i == 1)
+            if (maxreallayers == 1)
             {
                 for (int m = 1; m <= overshoot; m++)
                 {
                     chosen_ones_counter[m] = 1;
                 }
             }
-            else if (i > 1)
+            else if (maxreallayers > 1)
             {
                 if (layer == 1)
                 {
-                     chosen_ones_counter[k] += 1;
+                     chosen_ones_counter[i] += 1;
                 }
-                else if (layer < i)
+                else if (layer < maxreallayers)
                 {
-                    chosen_ones_counter[k] += layercontents.at(layer-1) / nmax;
+                    chosen_ones_counter[i] += layercontents.at(layer-1) / nmax;
                 }
-                else if (layer == i)
+                else if (layer == maxreallayers)
                 {
-                    if (remainder <= (layercontents.at(i-1)) / nmax && remainder != 0)
+                    if (remainder <= (layercontents.at(maxreallayers-1)) / nmax && remainder != 0)
                     {
-                        chosen_ones_counter[k] += quotient+1 * remainder;
+                        chosen_ones_counter[i] += quotient+1 * remainder;
                         remainder = 0;
                         break;
                     }
-                    else if (remainder > (layercontents.at(i-1)) / nmax)
+                    else if (remainder > (layercontents.at(maxreallayers-1)) / nmax)
                     {
-                        chosen_ones_counter[k] += quotient+1 * (layercontents.at(i-1) / nmax);
-                        remainder -= (layercontents.at(i-1) / nmax);
+                        chosen_ones_counter[i] += quotient+1 * (layercontents.at(maxreallayers-1) / nmax);
+                        remainder -= (layercontents.at(maxreallayers-1) / nmax);
                         break;
                     }
                     else
                     {
-                        chosen_ones_counter[k] += quotient * (layercontents.at(i-1) / nmax);
+                        chosen_ones_counter[i] += quotient * (layercontents.at(maxreallayers-1) / nmax);
                         break;
                     }
                 }
             }
         }
-        // std::cout << "coc: " << chosen_ones_counter[k] << std::endl;
-        total_counter += chosen_ones_counter[k];
+        // std::cout << "coc: " << chosen_ones_counter[i] << std::endl;
+        // total_counter += chosen_ones_counter[i];
     }
     std::cout << "amount of peers to verify: " << total_counter << std::endl;
 
