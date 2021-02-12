6e95bdb12f629df35ea1ccda89a91393179fef3f
nvrrdt <nicoverrijdt@gmail.com>
2021-02-12 11:29:50 +0100

    Ecdsa code is added and it compiles

--- include/libcommon/crypto.hpp
+++ include/libcommon/crypto.hpp
@@ -1,22 +1,78 @@
 #pragma once
 
+#include "stdafx.h"
+
+#include <assert.h>
+
 #include <iostream>
+using std::cout;
+using std::endl;
+
 #include <string>
+using std::string;
+
+#include "osrng.h"
+// using CryptoPP::AutoSeededX917RNG;
+using CryptoPP::AutoSeededRandomPool;
+
+#include "aes.h"
+using CryptoPP::AES;
+
+#include "integer.h"
+using CryptoPP::Integer;
+
+#include "sha.h"
+using CryptoPP::SHA256;
+
+#include "filters.h"
+using CryptoPP::StringSource;
+using CryptoPP::StringSink;
+using CryptoPP::ArraySink;
+using CryptoPP::SignerFilter;
+using CryptoPP::SignatureVerificationFilter;
+
+#include "files.h"
+using CryptoPP::FileSource;
+using CryptoPP::FileSink;
+
+#include "eccrypto.h"
+using CryptoPP::ECDSA;
+using CryptoPP::ECP;
+using CryptoPP::DL_GroupParameters_EC;
+
+#if _MSC_VER <= 1200 // VS 6.0
+using KeyPair = CryptoPP::ECDSA<ECP, SHA256>;
+using GroupParams = CryptoPP::DL_GroupParameters_EC<ECP>;
+#endif
+
+#include "oids.h"
+using CryptoPP::OID;
 
 namespace Crowd
 {
     class Crypto
     {
     public:
-        std::string create_sha256(std::string &msg);
-        std::string base58_encode(std::string &msg);
+        std::string sha256_create(std::string &msg);
+        std::string base58_encode_sha256(std::string &hash);
         std::string base58_decode(std::string &b58);
-        // ecdsa::Key generate_and_save_keypair();
+        int ecdsa_generate_and_save_keypair();
+        bool ecdsa_generate_private_key( const OID& oid, ECDSA<ECP, SHA256>::PrivateKey& key );
+        bool ecdsa_generate_public_key( const ECDSA<ECP, SHA256>::PrivateKey& ecdsa_private_key_, ECDSA<ECP, SHA256>::PublicKey& ecdsa_public_key_ );
+        void ecdsa_save_private_key( const ECDSA<ECP, SHA256>::PrivateKey& key );
+        void ecdsa_save_public_key( const ECDSA<ECP, SHA256>::PublicKey& key );
+        void ecdsa_load_private_key( ECDSA<ECP, SHA256>::PrivateKey& key );
+        void ecdsa_load_public_key( ECDSA<ECP, SHA256>::PublicKey& key );
+        bool ecdsa_sign_message( const ECDSA<ECP, SHA256>::PrivateKey& key, const string& message, string& signature );
+        bool ecdsa_verify_message( const ECDSA<ECP, SHA256>::PublicKey& key, const string& message, const string& signature );
         // ecdsa::Key get_keypair_with_priv_key(std::string &priv_key);
         // std::string get_priv_key();
         // std::string get_pub_key();
         // std::tuple<std::vector<uint8_t>, bool> sign(const std::string &string);
         // bool verify(const std::string &string, std::string &signature_base58, std::string &pub_key_base58);
     private:
+        // Private and Public keys
+        ECDSA<ECP, SHA256>::PrivateKey ecdsa_private_key_;
+        ECDSA<ECP, SHA256>::PublicKey ecdsa_public_key_;
     };
 }
--- src/libcommon/crypto.cpp
+++ src/libcommon/crypto.cpp
@@ -1,5 +1,6 @@
 #include "crypto.hpp"
 #include "base58.hpp"
+#include "configdir.hpp"
 
 #include "cryptlib.h"
 #include "sha3.h"
@@ -9,19 +10,19 @@ using namespace Crowd;
 using namespace CryptoPP;
 using namespace base58;
 
-std::string Crypto::create_sha256(std::string &msg)
+std::string Crypto::sha256_create(std::string &msg)
 {
     std::string digest;
 
-    SHA3_256 hash;
+    SHA256 hash;
     StringSource(msg, true, new HashFilter(hash, new StringSink(digest)));
 
     return digest;
 }
 
-std::string Crypto::base58_encode(std::string &msg)
+std::string Crypto::base58_encode_sha256(std::string &hash)
 {
-    std::string str = create_sha256(msg);
+    std::string str = sha256_create(hash);
     std::vector<uint8_t> vec(str.begin(), str.end());
     return base58::EncodeBase58(vec);
 }
@@ -41,4 +42,128 @@ std::string Crypto::base58_decode(std::string &b58)
     }
 }
 
-// ecdsa and rsa next
+int Crypto::ecdsa_generate_and_save_keypair()
+{
+    // Scratch result
+    bool result = false; 
+
+    // Generate Keys
+    result = ecdsa_generate_private_key( CryptoPP::ASN1::secp160r1(), ecdsa_private_key_ );
+    assert( true == result );
+    if( !result ) { return -1; }
+
+    result = ecdsa_generate_public_key( ecdsa_private_key_, ecdsa_public_key_ );
+    assert( true == result );
+    if( !result ) { return -2; }
+
+    return 0;
+}
+
+bool Crypto::ecdsa_generate_private_key( const OID& oid, ECDSA<ECP, SHA256>::PrivateKey& key )
+{
+    AutoSeededRandomPool prng;
+
+    key.Initialize( prng, oid );
+    assert( key.Validate( prng, 3 ) );
+     
+    return key.Validate( prng, 3 );
+}
+
+bool Crypto::ecdsa_generate_public_key( const ECDSA<ECP, SHA256>::PrivateKey& ecdsa_private_key_, ECDSA<ECP, SHA256>::PublicKey& ecdsa_public_key_ )
+{
+    AutoSeededRandomPool prng;
+
+    // Sanity check
+    assert( ecdsa_private_key_.Validate( prng, 3 ) );
+
+    ecdsa_private_key_.MakePublicKey(ecdsa_public_key_);
+    assert( ecdsa_public_key_.Validate( prng, 3 ) );
+
+    return ecdsa_public_key_.Validate( prng, 3 );
+}
+
+void Crypto::ecdsa_save_private_key( const ECDSA<ECP, SHA256>::PrivateKey& key )
+{
+    ConfigDir cd;
+    if (!boost::filesystem::exists(cd.GetConfigDir() + "ecdsa_priv_key"))
+    {
+        const std::string filename = cd.GetConfigDir() + "ecdsa_priv_key";
+        key.Save( FileSink( filename.c_str(), true /*binary*/ ).Ref() );
+    }
+    else
+    {
+        std::cout << "Ecdsa_priv_key existed already!!" << std::endl;
+    }
+}
+
+void Crypto::ecdsa_save_public_key( const ECDSA<ECP, SHA256>::PublicKey& key )
+{
+    ConfigDir cd;
+    if (!boost::filesystem::exists(cd.GetConfigDir() + "ecdsa_pub_key"))
+    {
+        const std::string filename = cd.GetConfigDir() + "ecdsa_pub_key";
+        key.Save( FileSink( filename.c_str(), true /*binary*/ ).Ref() );
+    }
+    else
+    {
+        std::cout << "Ecdsa_pub_key existed already!!" << std::endl;
+    }
+}
+
+void Crypto::ecdsa_load_private_key( ECDSA<ECP, SHA256>::PrivateKey& key )
+{
+    ConfigDir cd;
+    if (boost::filesystem::exists(cd.GetConfigDir() + "ecdsa_priv_key"))
+    {
+        const std::string filename = cd.GetConfigDir() + "ecdsa_priv_key";
+        key.Load( FileSource( filename.c_str(), true /*pump all*/ ).Ref() );
+    }
+    else
+    {
+        std::cout << "Ecdsa_priv_key doesn't exist!!" << std::endl;
+    }
+}
+
+void Crypto::ecdsa_load_public_key( ECDSA<ECP, SHA256>::PublicKey& key )
+{
+    ConfigDir cd;
+    if (boost::filesystem::exists(cd.GetConfigDir() + "ecdsa_pub_key"))
+    {
+        const std::string filename = cd.GetConfigDir() + "ecdsa_pub_key";
+        key.Load( FileSource( filename.c_str(), true /*pump all*/ ).Ref() );
+    }
+    else
+    {
+        std::cout << "Ecdsa_pub_key doesn't exist!!" << std::endl;
+    }
+}
+
+bool Crypto::ecdsa_sign_message( const ECDSA<ECP, SHA256>::PrivateKey& key, const string& message, string& signature )
+{
+    AutoSeededRandomPool prng;
+    
+    signature.erase();    
+
+    StringSource( message, true,
+        new SignerFilter( prng,
+            ECDSA<ECP,SHA256>::Signer(key),
+            new StringSink( signature )
+        ) // SignerFilter
+    ); // StringSource
+    
+    return !signature.empty();
+}
+
+bool Crypto::ecdsa_verify_message( const ECDSA<ECP, SHA256>::PublicKey& key, const string& message, const string& signature )
+{
+    bool result = false;
+
+    StringSource( signature+message, true,
+        new SignatureVerificationFilter(
+            ECDSA<ECP,SHA256>::Verifier(key),
+            new ArraySink( (byte*)&result, sizeof(result) )
+        ) // SignatureVerificationFilter
+    );
+
+    return result;
+}
