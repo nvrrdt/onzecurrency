993452f2bb3bde0a9572be4c8f5f9cde02d850d7
nvrrdt <nicoverrijdt@gmail.com>
2021-09-25 12:16:56 +0200

    Too soon 'this' rocksy was filled with its data, had to be after send_your_full_hash. This leaded to a wrong 'coordinator there and in inform_chosen_ones_final_block

--- src/libpoco/block_matrix.cpp
+++ src/libpoco/block_matrix.cpp
@@ -457,6 +457,7 @@ void BlockMatrix::save_final_block_to_file()
             }
 
             // actual saving to rocksdb
+            std::map<std::string, std::string> store_rocksdb_here; 
             for (uint16_t j = 0; j < intro_msg_s_mat_.get_intro_msg_s_3d_mat().at(i+1).at(0).size(); j++)
             {
                 nlohmann::json m_j;
@@ -481,10 +482,8 @@ void BlockMatrix::save_final_block_to_file()
                 rocksdb_j["rsa_pub_key"] = m_j["rsa_pub_key"];
                 std::string rocksdb_s = rocksdb_j.dump();
 
-                // Store to rocksdb for coordinator
-                Crowd::Rocksy* rocksy = new Crowd::Rocksy("usersdb");
-                rocksy->Put(full_hash_req, rocksdb_s);
-                delete rocksy;
+                // Store to vector to send later
+                store_rocksdb_here[full_hash_req] = rocksdb_s;
 
                 m_j["rocksdb"] = rocksdb_j;
                 std::shared_ptr<nlohmann::json> ptr = std::make_shared<nlohmann::json> (m_j);
@@ -500,6 +499,18 @@ void BlockMatrix::save_final_block_to_file()
             Poco::PocoCrowd pc;
             pc.send_your_full_hash(i+1, final_block_j, new_block_nr);
             pc.inform_chosen_ones_final_block(final_block_j, new_block_nr, for_rocksdb_j);
+
+            // save rocksdb here
+            for ( auto r : store_rocksdb_here )
+            {
+                std::string f = r.first;
+                std::string s = r.second;
+                
+                // Store to rocksdb for coordinator
+                Crowd::Rocksy* rocksy = new Crowd::Rocksy("usersdb");
+                rocksy->Put(f, s);
+                delete rocksy;
+            }
         }
     }
 
