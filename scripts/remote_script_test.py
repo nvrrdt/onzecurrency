2f293a0179b9c3b32e7cd88885788766c5792213
nvrrdt <nicoverrijdt@gmail.com>
2021-10-14 11:26:54 +0200

    The user of the remote_script_test gets indexed and those scripts get remotely executed, send_to_cin is just for memory

--- scripts/remote_script_test.py
+++ scripts/remote_script_test.py
@@ -3,10 +3,9 @@ import os
 import subprocess
 
 def main():
-    subprocess.call('echo it works && clear && echo -e "\033c\e[3J"' \
-        ' &&  dpkg -i ./onzecurrency-0.1.1-Linux.deb && apt-get -f install' \
+    subprocess.call('dpkg -i ./onzecurrency-0.1.1-Linux.deb && apt-get -f install' \
         ' && rm -rf /onzecurrency/.config' \
-        ' && onze-terminal', shell=True)
+        ' && echo er@er.c0 | onze-terminal', shell=True)
 
 if __name__ == '__main__':
     try:
--- scripts/system_tester.py
+++ scripts/system_tester.py
@@ -14,18 +14,51 @@ import sys
 import os
 import subprocess
 from pathlib import Path
+import time
+import queue, threading
 
 def main():
     # Cd to scripts/build.py directory
     os.chdir(os.path.dirname(__file__))
 
-    ips = ["51.158.68.232", "51.15.226.67", "51.15.248.67", "212.47.254.170", "212.47.234.94", "212.47.236.102"]
+    ips = ["51.158.68.232", "51.15.226.67"] # , "51.15.248.67", "212.47.254.170", "212.47.234.94", "212.47.236.102"]
     
+    string = 'er@er.c0'
+
     for ip in ips:
+        with open('remote_script_test.py', 'r') as file :
+            filedata = file.read()
+        filedata = filedata.replace(string, string[:7] + str(ips.index(ip)) + string[7 + 1:])
+        string = string[:7] + str(ips.index(ip)) + string[7 + 1:]
+        with open('remote_script_test.py', 'w') as file:
+            file.write(filedata)
+        file.close()
+
         subprocess.call('cd ' + project_path("build") + \
             ' && scp `find . -maxdepth 1 -type f -name *.deb` root@' + ip + ':~/' + \
             ' && cd ' + project_path("scripts") + \
-            ' && ssh root@' + ip + ' nohup python3 remote_script_test.py', shell=True)
+            ' && scp remote_script_test.py root@' + ip + ':~/', shell=True)
+
+    with open('remote_script_test.py', 'r') as file :
+        filedata = file.read()
+    filedata = filedata.replace(string, 'er@er.c0')
+    with open('remote_script_test.py', 'w') as file:
+        file.write(filedata)
+    file.close()
+
+    q = queue.Queue()
+    for ip in ips:
+        q.put(ip)
+
+    threads = [ threading.Thread(target=worker, args=(q,)) for _i in range(len(ips)) ]
+    for thread in threads:
+        thread.start()
+
+def worker(q):
+    ip = q.get()
+    subprocess.call('ssh root@' + ip + ' nohup python3 remote_script_test.py', shell=True)
+    time.sleep(20) # wait 20 seconds == block creation delay
+    return
 
 def project_path(sub_dir):
     # find the path of the sub_dir folder in the project folder
