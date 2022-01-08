6a33bb40045f3e090024327eca59a4a86b81f18d
nvrrdt <nicoverrijdt@gmail.com>
2022-01-08 16:02:53 +0100

    These breaks aren't necessary

--- src/libpoco/poco_crowd.cpp
+++ src/libpoco/poco_crowd.cpp
@@ -345,8 +345,6 @@ void PocoCrowd::inform_chosen_ones_prel_block(std::string my_next_block_nr, nloh
         std::string v;
         for (auto &[k, v] : parts)
         {
-            if (sync.get_break_block_creation_loops()) break;
-
             if (v == "0" || v == "") break; // TODO the parts need to be refactored everywhere as it's an ugly hack
 pl.handle_print_or_log({"__003", "chosen_ones sent", v});
             message_j["chosen_ones"].push_back(v);
@@ -373,8 +371,6 @@ pl.handle_print_or_log({"__003", "chosen_ones sent", v});
         std::string key, val;
         for (auto &[key, val] : parts)
         {
-            if (sync.get_break_block_creation_loops()) break;
-
             if (key == 1) continue;
             if (val == my_full_hash || val == "") continue; // UGLY: sometimes it's "" and sometimes "0" --> should be one or the other
             
