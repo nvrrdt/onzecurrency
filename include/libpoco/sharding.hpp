7c4e14508db099da46baa9418ba4395fd42978b0
nvrrdt <nicoverrijdt@gmail.com>
2022-04-09 10:40:46 +0200

    this one must also be user_id

--- include/libpoco/sharding.hpp
+++ include/libpoco/sharding.hpp
@@ -17,7 +17,7 @@ namespace Poco
         
     private:
         std::map<std::string, uint256_t> fair_partitioning();
-        std::pair<std::string, uint256_t> get_fair_order_nr(std::string user_id);
+        std::pair<std::string, uint256_t> get_fair_user_id(std::string user_id);
         uint32_t get_amount_of_shards();
         uint256_t get_shard_distance();
     };
