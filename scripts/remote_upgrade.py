61f06c27a562f44933db25931310ddf20d5084c7
nvrrdt <nicoverrijdt@gmail.com>
2022-07-24 16:49:25 +0200

    Update those systems in threads

--- scripts/remote_upgrade.py
+++ scripts/remote_upgrade.py
@@ -1,13 +1,33 @@
 import sys
 import os
 import subprocess
+import threading
+import queue
 
 def main():
     ips = ["212.47.231.236", "51.15.226.67", "51.15.248.67", "212.47.254.170", "212.47.234.94", "212.47.236.102"]
     #ips = ["51.15.248.67"]
 
+    # Queue to use in the threads
+    q = queue.Queue()
     for ip in ips:
-        subprocess.call('ssh root@' + ip + ' "apt autoremove -y && apt update && apt upgrade -y && reboot"', shell=True)
+        q.put(ip)
+
+    # Creating a thread for each server
+    threads = [ threading.Thread(target=worker, args=(q,)) for _i in range(len(ips)) ]
+    for thread in threads:
+        thread.start()
+
+    # Wait for all of them to finish
+    for t in threads:
+        t.join()
+
+    # Notify the user
+    subprocess.call('notify-send -t 2000 "Done"', shell=True)
+
+def worker(q):
+    ip = q.get()
+    subprocess.call('ssh root@' + ip + ' "apt autoremove -y && apt update && apt upgrade -y && reboot"', shell=True)
 
 if __name__ == '__main__':
     try:
